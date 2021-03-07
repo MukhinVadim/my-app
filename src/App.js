b4b4be0ae0e16e93f15c7e9ad4b0c5fe4c8a2611
Mukhin Vadim <mukhinvadim@gmail.com>
2021-03-07 18:54:30 +0400

    ver 1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from "react";
+
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+const App = () => {
+    return (
+        <div className="app-wrapper">
+            <Header></Header>
+            <Navbar></Navbar>
+            <Profile></Profile>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="header-logo-img"
+                 src="https://img.icons8.com/ios/452/pear.png"
+                 alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
